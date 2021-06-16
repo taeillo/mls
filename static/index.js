afce00e12b42e075d687b943475dec050732d2e2
Admin <51963574+taeillo@users.noreply.github.com>
2021-06-16 17:08:56 +0100

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,13 @@
+function openNav(){
+	menu = document.getElementById("overlay");
+	menu.style.width = "300px";
+	menu.style.display = "flex"
+
+}
+
+function closeNav(){
+	menu = document.getElementById("overlay");
+	menu.style.width = "0";
+	menu.style.display = "none"
+
+}
